5ad7fbcc13e26bc9ecbb88b2ed57c1b9b74b8667
phiiil <phiiil3737@gmail.com>
2021-05-02 21:17:26 -0400

    Challenge 2 - Done - verify and sign

--- client/index.js
+++ client/index.js
@@ -1,4 +1,10 @@
 import "./index.scss";
+import { ec } from 'elliptic'
+import sha256 from 'crypto-js/sha256';
+
+console.log(ec);
+var elliptic = new ec('secp256k1');
+console.log(elliptic);
 
 const server = "http://localhost:3042";
 
@@ -17,21 +23,33 @@ document.getElementById("exchange-address").addEventListener('input', ({ target:
 
 document.getElementById("transfer-amount").addEventListener('click', () => {
   const sender = document.getElementById("exchange-address").value;
-
-  const pk = document.getElementById("pk").value;
-  // sign here
   const amount = document.getElementById("send-amount").value;
   const recipient = document.getElementById("recipient").value;
-
-  const body = JSON.stringify({
-    sender, amount, recipient, signature
-  });
-
-  const request = new Request(`${server}/send`, { method: 'POST', body });
-
-  fetch(request, { headers: { 'Content-Type': 'application/json' } }).then(response => {
-    return response.json();
-  }).then(({ balance }) => {
-    document.getElementById("balance").innerHTML = balance;
-  });
+  // transfer request
+  const transferRequest = { sender, amount, recipient }
+  // sign request with private key
+  if (sender && amount && recipient) {
+    const privateKey = document.getElementById("private-key").value;
+    const key = elliptic.keyFromPrivate(privateKey);
+    const transferRequestHash = sha256(JSON.stringify(transferRequest));
+    const signature = key.sign(transferRequestHash.toString());
+    console.log(signature.toString());
+    const body = JSON.stringify({ transferRequest, signature });
+    console.log(body);
+    // send request to the server
+    const request = new Request(`${server}/send`, { method: 'POST', body });
+    fetch(request, { headers: { 'Content-Type': 'application/json' } }).then(response => {
+      return response.json();
+    }).then(({ message, balance }) => {
+      document.getElementById("balance").innerHTML = balance;
+      document.getElementById("transfer-result").innerHTML = message;
+    });
+  }
+  else {
+    document.getElementById("transfer-result").innerHTML = "Missing values";
+  }
 });
+
+window.onload = () => {
+  //document.getElementById("exchange-address").value = "1234";
+}
--- server/index.js
+++ server/index.js
@@ -3,6 +3,11 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = 3042;
+// sign and verify
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
+const SHA256 = require("crypto-js/sha256");
+
 
 // localhost can have cross origin errors
 // depending on the browser you use!
@@ -24,15 +29,40 @@ const accounts = a.generateAccounts(balances);
 app.get('/balance/:address', (req, res) => {
   const { address } = req.params;
   console.log(`balance request: ${address}`);
-  const balance = accounts.get(address) || 0;
+  const balance = accounts.get(address) || "invalid account";
   res.send({ balance });
 });
 
 app.post('/send', (req, res) => {
-  const { sender, recipient, amount } = req.body;
-  balances[sender] -= amount;
-  balances[recipient] = (balances[recipient] || 0) + +amount;
-  res.send({ balance: balances[sender] });
+  console.log(`Transfer Request:`)
+  console.log(req.body)
+  const { transferRequest, signature } = req.body;
+  const { sender, amount, recipient } = transferRequest;
+  // verify signature against transferRequest
+  // transfertRequest was signed with private key, verify against sender public key
+  const key = ec.keyFromPublic(sender, 'hex');
+  const transferRequestHashString = SHA256(JSON.stringify(transferRequest)).toString();
+  const verified = key.verify(transferRequestHashString, signature);
+  console.log(`Signature verified: ${verified}`);
+  if (verified) {
+    const senderBalance = accounts.get(sender) || 0;;
+    if (senderBalance >= amount) {
+      // you have enough coins, transfer
+      accounts.set(sender, senderBalance - amount);
+      recipientBalance = accounts.get(recipient);
+      accounts.set(recipient, recipientBalance + amount);
+      const response = { verified, message: "Transfer successful", balance: accounts.get(sender) };
+      res.send(response);
+    }
+    else {
+      const response = { verified, message: "Transfer failed. Not enough coins.", balance: accounts.get(sender) };
+      res.send(response);
+    }
+  }
+  else {
+    const response = { verified, message: "Transfer failed. Invalid signature.", balance: accounts.get(sender) };
+    res.send(response);
+  }
 });
 
 
